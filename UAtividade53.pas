fda939dea4567172aab1d36bbe27f1bc08ffd5a6
carloseduardosalasrivas-1202 <carloseduardosalasrivas-1202>
2026-05-21 20:24:10 -0300

    sla5

---
+++ UAtividade53.pas
@@ -0,0 +1,46 @@
+unit Ucomponentes;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TfrmCadastro = class(TForm)
+    edtNome: TEdit;
+    edtEndereco: TEdit;
+    lblNome: TLabel;
+    lblEndereço: TLabel;
+    gpbSexo: TGroupBox;
+    rbtMasc: TRadioButton;
+    rbtFem: TRadioButton;
+    cbbCidade: TComboBox;
+    gpbFrutas: TGroupBox;
+    ccbMaca: TCheckBox;
+    ccbBanana: TCheckBox;
+    ccbUva: TCheckBox;
+    ccbMorango: TCheckBox;
+    ccbJaca: TCheckBox;
+    ccbManga: TCheckBox;
+    mmoRegistros: TMemo;
+    btnCadastrar: TButton;
+    btnLimpar: TButton;
+  private
+    { Private declarations }
+    nRegistro : Integer;
+
+  public
+    { Public declarations }
+  end;
+
+var
+  frmCadastro: TfrmCadastro;
+
+implementation
+
+{$R *.dfm}
+
+uses USexo, UCidade;
+
+end.
